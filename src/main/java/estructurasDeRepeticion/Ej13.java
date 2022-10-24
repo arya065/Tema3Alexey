017d532b4c657395c72b7c2d87bd39efe4fd900b
arya065 <arya065@g.educaand.es>
2022-10-24 13:33:10 +0200

    add all repeticion

--- src/main/java/estructurasDeRepeticion/Ej13.java
+++ src/main/java/estructurasDeRepeticion/Ej13.java
@@ -3,11 +3,11 @@ package estructurasDeRepeticion;
 public class Ej13 {
 
     public static void main(String[] args) {
-        int b = 65;
+        //int b = 65;
         for (char a = 'A'; a <= 'Z'; a++) {
 
-            System.out.println(a + "-" + b);
-            b++;
+            System.out.println(a + "-" + (int)a);
+            //b++;
         }
     }
 }
